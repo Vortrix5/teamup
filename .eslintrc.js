790f79916cc1be80c607a65acbdc9b3ff1d79957
Vortrix <amine.zouaoui2011@gmail.com>
2024-02-09 07:41:57 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off',
+  }
+}
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar app color="primary">
+    <v-toolbar-title>TeamUp</v-toolbar-title>
+    <v-spacer></v-spacer>
+      <v-toolbar-items class="d-flex align-center">
+        <v-btn v-for="item in centerItems" :key="item.text" text class="gc-5" >
+          <v-icon left class="mr-2">{{ item.icon }}</v-icon>
+          {{ item.text }}
+        </v-btn>
+      </v-toolbar-items>
+    <v-toolbar-items class="justify-end">
+      <v-btn text class="gc-5">
+        <v-icon left class="mr-2">mdi-account</v-icon>
+        Login
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const centerItems = ref([
+  { text: 'Home', icon: 'mdi-home' },
+  { text: 'About', icon: 'mdi-information' },
+  { text: 'Contact', icon: 'mdi-email' },
+  { text: 'Testimonials', icon: 'mdi-star' },
+]);
+</script>
+
+<style scoped>
+</style>
